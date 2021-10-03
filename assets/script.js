8c5f2130ede1da94a02210002a80913bd2d1d9a6
jessicamcg <jessicamcguico@gmail.com>
2021-10-03 18:22:14 -0500

    rendering previous serach history as buttons, no function yet

--- assets/script.js
+++ assets/script.js
@@ -3,11 +3,27 @@
 
 var cityNameInputForm = $('#input-city-name');
 var cityInfoEl = $('#city-card');
-var today = moment();
 
-// function init() {
-//     //localStorage.getItem()
-// };
+var currentCity = document.createElement('h2');
+var currentTemp = document.createElement('p');
+var currentWind = document.createElement('p');
+var currentHumidity = document.createElement('p');
+var currentUVIndex = document.createElement('p');
+var futureDate = document.createElement('h6')
+var icon = document.createElement('img')
+var futureTemp = document.createElement('p');
+var futureWind = document.createElement('p');
+var futureHumidity = document.createElement('p');
+var savedBtns;    
+
+var today = moment();
+var savedCities;
+
+function init() {
+    // savedCities = JSON.parse(localStorage.getItem('savedCities')) || [];
+    savedCities = [];
+    console.log(savedCities);
+};
 
 function handleSubmitFormSearch(event) {
 
@@ -34,11 +50,11 @@ function getCityWeather(city) {
 };
 
 function renderCityWeather(data) {
-
-    var currentCity = document.createElement('h2');
-    var currentTemp = document.createElement('p');
-    var currentWind = document.createElement('p');
-    var currentHumidity = document.createElement('p');
+    
+    currentCity.textContent = '';
+    currentTemp.textContent = '';
+    currentWind.textContent = '';
+    currentHumidity.textContent = '';
 
     currentCity.textContent = data.name + ' (' + today.format("L") +') ';
     currentTemp.textContent = 'Temp: ' + data.main.temp +'°F';
@@ -51,6 +67,7 @@ function renderCityWeather(data) {
     cityInfoEl.append(currentHumidity);
     cityInfoEl.addClass('current-city');
     
+    saveSearch(data.name);
 };
 
 function getForecast(lat,lon) {
@@ -67,38 +84,69 @@ function getForecast(lat,lon) {
 };
 
 function renderForecast(data) {
-    console.log(data);
-    var currentUVIndex = document.createElement('p');
+    currentUVIndex.textContent = '';
     currentUVIndex.textContent = 'UV Index: ' + data.current.uvi;
     cityInfoEl.append(currentUVIndex);
     
     $('#5-day-forecast').text('5-Day-Forecast');
-
     for (var i=1; i<6; i++) {
-        var futureDate = document.createElement('h6')
-        var icon = document.createElement('img')
-        var futureTemp = document.createElement('p');
-        var futureWind = document.createElement('p');
-        var futureHumidity = document.createElement('p');        
-        var iconCode = data.daily[i].weather[0].icon;
+        futureDate.textContent = '';
+        futureTemp.textContent =  '';
+        futureWind.textContent = '';
+        futureHumidity.textContent = '';
+        $('#day-'+i).empty();
+    };
+    
+    for (var j=1; j<6; j++) {
+
+        futureDate = document.createElement('h6')
+        icon = document.createElement('img')
+        futureTemp = document.createElement('p');
+        futureWind = document.createElement('p');
+        futureHumidity = document.createElement('p');
+        var iconCode = data.daily[j].weather[0].icon;
         var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
         icon.setAttribute('src',iconUrl)
+        today = moment();
+        futureDate.textContent = today.add(j, 'days').format('L');
+        futureTemp.textContent = 'Temp: ' +  data.daily[j].temp.day +'°F';
+        futureWind.textContent =  'Wind: ' + data.daily[j].wind_speed + 'MPH';
+        futureHumidity.textContent = 'Humidity: ' + data.daily[j].humidity + '%';
+        
+        $('#day-'+j).append(futureDate);
+        $('#day-'+j).append(icon);
+        $('#day-'+j).append(futureTemp);
+        $('#day-'+j).append(futureWind);
+        $('#day-'+j).append(futureHumidity);
+        
+    };
 
-        
-        futureDate.textContent = today.add(1, 'days').format('L');
-        futureTemp.textContent = 'Temp: ' +  data.daily[i].temp.day +'°F';
-        futureWind.textContent =  'Wind: ' + data.daily[i].wind_speed + 'MPH';
-        futureHumidity.textContent = 'Humidity: ' + data.daily[i].humidity + '%';
-        
-        $('#day-'+i).append(futureDate);
-        $('#day-'+i).append(icon);
-        $('#day-'+i).append(futureTemp);
-        $('#day-'+i).append(futureWind);
-        $('#day-'+i).append(futureHumidity);
-        
+};
+
+function saveSearch(city) {
+    // city = [city];
+    savedCities.push(city)
+    localStorage.setItem('savedCities',JSON.stringify(city));
+    renderSearchHistoryBtn();
+};
+
+function renderSearchHistoryBtn() {
+    // make buttons using city names saved in local storage
+    
+    console.log(typeof savedCities);
+    console.log(savedCities);
+    console.log(savedCities.length);
+    $('aside').children().remove('button');
+    for (var k=0; k<savedCities.length;k++) {
+        savedBtns = document.createElement('button')
+        savedBtns.setAttribute('class', 'btn btn-secondary w-100');
+        savedBtns.textContent = savedCities[k];
+        console.log(savedCities[k]);
+        $('aside').append(savedBtns);
     };
 };
 
 $('#search-form-btn').on('click', handleSubmitFormSearch)
+//make event listener for search history buttons
 
-// init();
+init();
